22e1d2f443f5c092bea4437384ba9af4c90464c5
A.S <A.S@DESKTOP-DPOE2IC>
2026-05-23 00:25:49 +0300

    Add project files.

---
+++ CaesarCipherApp/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace CaesarCipherApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtShift = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LightCoral;
+            this.label1.Location = new System.Drawing.Point(43, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PlainText";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.LightCoral;
+            this.label2.Location = new System.Drawing.Point(44, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Shift Key";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.LightCoral;
+            this.label3.Location = new System.Drawing.Point(44, 290);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Result";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(47, 101);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(100, 31);
+            this.txtInput.TabIndex = 4;
+            // 
+            // txtShift
+            // 
+            this.txtShift.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtShift.Location = new System.Drawing.Point(48, 174);
+            this.txtShift.Name = "txtShift";
+            this.txtShift.Size = new System.Drawing.Size(100, 31);
+            this.txtShift.TabIndex = 5;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Century Schoolbook", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Location = new System.Drawing.Point(47, 325);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(166, 29);
+            this.txtResult.TabIndex = 6;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.ForeColor = System.Drawing.Color.LightCoral;
+            this.btnEncrypt.Location = new System.Drawing.Point(22, 225);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 45);
+            this.btnEncrypt.TabIndex = 7;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.ForeColor = System.Drawing.Color.LightCoral;
+            this.btnDecrypt.Location = new System.Drawing.Point(133, 225);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 45);
+            this.btnDecrypt.TabIndex = 8;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.ForeColor = System.Drawing.Color.LightCoral;
+            this.btnCopy.Location = new System.Drawing.Point(250, 236);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 9;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.ForeColor = System.Drawing.Color.LightCoral;
+            this.btnClear.Location = new System.Drawing.Point(179, 371);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(72, 23);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.LightCoral;
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(242, 38);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "CaesarCipherTool";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtShift);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CaesarCipherEncryptionTool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtShift;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ CaesarCipherApp/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaesarCipherApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            string text = txtInput.Text.ToUpper();
+
+            if (!int.TryParse(txtShift.Text, out int shift))
+            {
+                MessageBox.Show("Enter a valid number");
+
+                return;
+            }
+
+            string result = "";
+
+            foreach (char c in text)
+            {
+                if (char.IsLetter(c))
+                {
+                    char encryptedChar =
+                        (char)(((c - 'A' + shift) % 26) + 'A');
+
+                    result += encryptedChar;
+                }
+                else
+                {
+                    result += c;
+                }
+            }
+
+            txtResult.Text = result;
+        }
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            string text = txtInput.Text.ToUpper();
+
+            if (!int.TryParse(txtShift.Text, out int shift))
+            {
+                MessageBox.Show("Enter a valid number");
+
+                return;
+            }
+
+            string result = "";
+
+            foreach (char c in text)
+            {
+                if (char.IsLetter(c))
+                {
+                    char decryptedChar =
+                        (char)(((c - 'A' - shift + 26) % 26) + 'A');
+
+                    result += decryptedChar;
+                }
+                else
+                {
+                    result += c;
+                }
+            }
+
+            txtResult.Text = result;
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txtResult.Text);
+
+            MessageBox.Show("Copied Successfully");
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtInput.Clear();
+
+            txtResult.Clear();
+
+            txtShift.Clear();
+
+            txtInput.Focus();
+        }
+    }
+}
